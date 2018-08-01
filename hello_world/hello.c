78815bada09d99f348a4c81abec9082f735ece7a
haxmanster <grzegorz-wolyniec@o2.pl>
2018-08-01 10:42:29 +0200

    Create first kernel module

---
+++ hello_world/hello.c
@@ -0,0 +1,14 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+ static int hello_init(void){
+     printk(KERN_ALERT "TEST: Hello world ,this is soliduscode\n");
+
+     return 0;
+ }
+ static void hello_exit(void){
+     printk(KERN_ALERT "TEST: Good byte, from soliduscode");
+ }
+
+ module_init(hello_init);
+ module_exit(hello_exit);
